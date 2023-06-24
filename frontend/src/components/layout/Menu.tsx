e0956e92bad7aa6c7cca72cede45133a5df173a1
slekup <74949101+slekup@users.noreply.github.com>
2023-06-25 00:35:01 +1200

    remove path import from menu

--- frontend/src/components/layout/Menu.tsx
+++ frontend/src/components/layout/Menu.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import path from 'path';
 import { useState } from 'react';
 
 import { HiMenu } from 'react-icons/hi';
